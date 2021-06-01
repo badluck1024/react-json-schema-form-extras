f9620f5328a01fc677da3805befc8c2d96249693
alessandroleo <alessandro.leo@weavesrl.com>
2021-06-01 12:39:27 +0200

    Added npm module htm-to-react

--- src/SimpleLabel.js
+++ src/SimpleLabel.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import renderHTML from "react-render-html";
+import ReactDOMServer from "react-dom/server";
+import { Parser as HtmlToReactParser } from "html-to-react";
 
 export default class SimpleLabel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      label: "",
+      label: ""
     };
   }
 
@@ -13,17 +14,21 @@ export default class SimpleLabel extends Component {
     let { schema: { title }, name } = this.props;
 
     this.setState({
-      label: title ? title : name,
+      label: title ? title : name
     });
   }
 
   render() {
     let {
-      uiSchema: { simpleLabel: { styles, classNames = "simpleLabel" } = {} },
+      uiSchema: { simpleLabel: { styles, classNames = "simpleLabel" } = {} }
     } = this.props;
+
+    const htmlToReactParser = new HtmlToReactParser();
+    const reactElement = htmlToReactParser.parse(this.state.label);
+    const reactHtml = ReactDOMServer.renderToStaticMarkup(reactElement);
     return (
       <span className={classNames} style={styles}>
-        {renderHTML(this.state.label)}
+        {reactHtml}
       </span>
     );
   }
